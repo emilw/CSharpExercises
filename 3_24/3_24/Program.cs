337c9411b4f86c45a48edfb892976c5f11be6696
Emil <emil@postback.se>
2017-05-15 20:11:15 +0200

    3_24

---
+++ 3_24/3_24/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int number = 0;
+                Console.WriteLine("Give me a number(or 'q' to quit):");
+                var input = Console.ReadLine();
+
+                if (String.Equals(input, "q"))
+                {
+                    break;
+                }
+                else if (!Int32.TryParse(input, out number))
+                {
+                    Console.WriteLine("The input was not a valid number");
+                    continue;
+                }
+
+                var rest = number % 2;
+
+                if (rest == 0)
+                    Console.WriteLine("It's even!");
+                else
+                    Console.WriteLine("It's odd!");
+            }
+            Console.WriteLine("Hit any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4_12/4_12/InvoiceLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_12
+{
+    public class InvoiceLine
+    {
+        public string PartNumber { get; set; }
+        public string PartDescription { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalAmount {
+            get
+            {
+                return Quantity * UnitPrice;
+            }
+        }
+    }
+}

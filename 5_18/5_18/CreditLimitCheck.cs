c26f65ceaa8e3133e6054016142a6071c8b4eb1e
Emil <emil@postback.se>
2017-05-16 14:26:29 +0200

    5.18

---
+++ 5_18/5_18/CreditLimitCheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_18
+{
+    public class CreditLimitCheck
+    {
+        public CreditLimitCheck(int accountNumber, int balanceInBeginningOfMonth, int totalChargeThisMonth, int totalCreditThisMonth, int creditLimit)
+        {
+            AccountNumber = accountNumber;
+            BalanceInBeginningOfMonth = balanceInBeginningOfMonth;
+            TotalChargeThisMonth = totalChargeThisMonth;
+            TotalCreditThisMonth = totalCreditThisMonth;
+            CreditLimit = creditLimit;
+        }
+
+        public int AccountNumber { get; private set; }
+        public int BalanceInBeginningOfMonth { get; private set; }
+        public int TotalChargeThisMonth { get; private set; }
+        public int TotalCreditThisMonth { get; private set; }
+        public int CreditLimit { get; private set; }
+
+        public bool IsCreditLimitExceeded
+        {
+            get
+            {
+                return TotalBalance < 0 ? true : false;
+            }
+        }
+
+        public int CurrentMonthlyBalance
+        {
+            get
+            {
+                return (BalanceInBeginningOfMonth + TotalChargeThisMonth - TotalCreditThisMonth);
+            }
+        }
+
+        public int TotalBalance
+        {
+            get
+            {
+                return CreditLimit + CurrentMonthlyBalance;
+            }
+        }
+    }
+}
---
+++ 5_18/5_18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the credit limit check program");
+            
+            while (true)
+            {
+                Console.WriteLine("Fill in the details below to get the current status for the customers credit limit");
+                Console.Write("Account number: ");
+                var accountNumber = Console.ReadLine();
+                Console.Write("Balance in the beginning of the month: ");
+                var balanceInTheBeginningOfTheMonth = Console.ReadLine();
+                Console.Write("Total charge so far this month: ");
+                var chargedSoFar = Console.ReadLine();
+                Console.Write("Total Credit so far this month: ");
+                var creditSoFar = Console.ReadLine();
+                Console.Write("Credit limit: ");
+                var creditLimit = Console.ReadLine();
+
+                var customerCredit = new CreditLimitCheck(Convert.ToInt32(accountNumber), Convert.ToInt32(balanceInTheBeginningOfTheMonth),
+                    Convert.ToInt32(chargedSoFar), Convert.ToInt32(creditSoFar), Convert.ToInt32(creditLimit));
+
+                if (customerCredit.IsCreditLimitExceeded)
+                    Console.WriteLine("Credit limit exceeded");
+
+                Console.WriteLine("Current monthly balance is {0} and the total balance is {1}", customerCredit.CurrentMonthlyBalance, customerCredit.TotalBalance);
+
+                Console.WriteLine("If you want to test a new customer account type 'y', otherwise 'q' to quit");
+
+                var decision = Console.ReadLine();
+                if (String.Equals(decision, "q"))
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Hit any key to exit");
+            Console.ReadLine();
+        }
+    }
+}

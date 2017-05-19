c1a2a5e70346543453098c8e20b4565bf7398623
Emil <emil@postback.se>
2017-05-19 20:15:26 +0200

    9.4

---
+++ 9_4/9_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Duplicate word removal!");
+
+            while (true)
+            {
+
+                Console.Write("Enter a sentence(-1 to quit): ");
+                var sentenceInput = Console.ReadLine();
+                if (String.Equals(sentenceInput, "-1"))
+                {
+                    break;
+                }
+
+                var words = sentenceInput.Split().Select(x => x.ToLower());
+
+                var onlySingleWords = words.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.First());
+
+                foreach (var word in onlySingleWords.OrderBy(x => x))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+            Console.WriteLine("Hit any key to exit");
+            Console.ReadLine();
+        }
+    }
+}

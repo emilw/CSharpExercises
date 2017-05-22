6768a632c510b47382e76af6e0333fee8c205abe
Emil <emil@postback.se>
2017-05-22 21:53:06 +0200

    16.4

---
+++ 16_4/16_4/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace _16_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValueA = new System.Windows.Forms.TextBox();
+            this.txtValueB = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtValueA
+            // 
+            this.txtValueA.Location = new System.Drawing.Point(12, 11);
+            this.txtValueA.Name = "txtValueA";
+            this.txtValueA.Size = new System.Drawing.Size(132, 20);
+            this.txtValueA.TabIndex = 0;
+            // 
+            // txtValueB
+            // 
+            this.txtValueB.Location = new System.Drawing.Point(150, 11);
+            this.txtValueB.Name = "txtValueB";
+            this.txtValueB.Size = new System.Drawing.Size(132, 20);
+            this.txtValueB.TabIndex = 1;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(194, 37);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(88, 23);
+            this.btnCompare.TabIndex = 2;
+            this.btnCompare.Text = "CompareTo()";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(16, 66);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(266, 20);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 99);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.txtValueB);
+            this.Controls.Add(this.txtValueA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValueA;
+        private System.Windows.Forms.TextBox txtValueB;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ 16_4/16_4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            var comparisonValue = txtValueA.Text.CompareTo(txtValueB.Text);
+            var textCode = "IS EQUAL TO";
+
+            if (comparisonValue > 0)
+                textCode = "IS GREATER THAN";
+            else if (comparisonValue < 0)
+                textCode = "IS LESS THAN";
+
+            txtResult.Text = String.Format("{0} {1} {2}", txtValueA.Text, textCode, txtValueB.Text);
+        }
+    }
+}

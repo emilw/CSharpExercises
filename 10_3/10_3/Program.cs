bf49d07c5e56749d71048de8fe6c70fa2bf80a69
Emil <emil@postback.se>
2017-05-22 10:00:56 +0200

    10.3

---
+++ 10_3/10_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Exercise 9.4 Solution: RectangleTest.cs 
+// Application tests class Rectangle. 
+using System;
+
+namespace _10_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            int choice = GetMenuChoice();
+
+            while (choice != 3)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter length: ");
+                        rectangle.Length = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.Write("Enter width: ");
+                        rectangle.Width = Convert.ToDouble(Console.ReadLine());
+                        break;
+                } // end switch 
+
+                Console.WriteLine(rectangle.ToString());
+                Console.WriteLine();
+
+                choice = GetMenuChoice();
+            } // end while 
+        } // end Main 
+
+        // prints a menu and returns a value corresponding to the menu choice 
+        private static int GetMenuChoice()
+        {
+            Console.WriteLine("1. Set Length");
+            Console.WriteLine("2. Set Width");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choice: ");
+
+            return Convert.ToInt32(Console.ReadLine());
+        } // end method GetMenuChoice 
+    } // end class RectangleTest
+}
---
+++ 10_3/10_3/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_3
+{
+    public class Rectangle
+    {
+        private double length = 1;
+        private double width = 1;
+
+        public double Length {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (isValid(value))
+                    length = value;
+            }
+        }
+        public double Width {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (isValid(value))
+                    width = value;
+            }
+        }
+        public double Perimiter
+        {
+            get
+            {
+                return (Length * 2) +  (Width*2);
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return Length * Width;
+            }
+        }
+
+        private bool isValid(double number)
+        {
+            return number < 20 && number > 0 ? true : false;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Length: {0} and Width: {1} \nArea: {2} \nPerimiter: {3}", Length, Width, Area, Perimiter);
+        }
+    }
+}

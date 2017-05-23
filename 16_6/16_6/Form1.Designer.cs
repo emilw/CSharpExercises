0f82a39da1282b9ff0e2a38eb53097fed3a86815
Emil <emil@postback.se>
2017-05-23 13:50:39 +0200

    16.6

---
+++ 16_6/16_6/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _16_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(26, 29);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(395, 88);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(26, 123);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(395, 96);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 242);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Piglatin translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ 16_6/16_6/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            var pigifiedText = "";
+            var shouldStartWithCapitalLetter = true;
+
+            foreach (var word in textBox.Text.Split().Where(x => !String.IsNullOrWhiteSpace(x)))
+            {
+                pigifiedText += String.Format("{0} ", GetPigLatin(word, shouldStartWithCapitalLetter));
+                shouldStartWithCapitalLetter = sentenceEndingCharacters.Contains(word.Last());
+            }
+
+            txtOutput.Text = pigifiedText;
+
+        }
+
+        private char[] sentenceEndingCharacters = { '.', '?', '!' };
+        private char[] endOfWordCharacters = { '.', '?', '!', ',' };
+
+        private string GetPigLatin(string word, bool startWithUpperCase)
+        {
+            if (word.Length > 1)
+            {
+                var firstLetter = word.First();
+                var lastCharacter = word.Last();
+                var rest = word.TrimStart(firstLetter);
+                if (endOfWordCharacters.Contains(lastCharacter))
+                    rest = rest.TrimEnd(lastCharacter);
+                var pigifiedWord = String.Format("{0}{1}{2}", rest, firstLetter, "ay").ToLower();
+
+                if (endOfWordCharacters.Contains(lastCharacter))
+                    pigifiedWord += lastCharacter;
+
+                if (startWithUpperCase)
+                {
+                    var capitalStartingLetter = pigifiedWord.First().ToString().ToUpper();
+                    pigifiedWord = pigifiedWord.Substring(1);
+                    pigifiedWord = String.Format("{0}{1}", capitalStartingLetter, pigifiedWord);
+                }
+
+                return pigifiedWord;
+            }
+            return word;
+        }
+    }
+}
